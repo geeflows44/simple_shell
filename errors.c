3a5b3beba14f241c59d8525884d3ac0d44406bca
manuelonyii <edvardjonas7@gnas7@gmail.com>
2022-11-19 07:43:54 -0800

    task

--- errors.c
+++ errors.c
@@ -21,7 +21,7 @@ i++;
 
 /**
 * _eputchar - writes the character c to stderr
-^ @c: The character to print
+* @c: The character to print
 *
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
